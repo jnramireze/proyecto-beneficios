81175b8b4800e883f7e310cdb46d967e06f1a25d
Juan Ramírez <116373329+jnramireze@users.noreply.github.com>
2026-02-19 16:37:59 -0300

    Create whitelist.py

---
+++ whitelist.py
@@ -0,0 +1,17 @@
+# whitelist.py
+from urllib.parse import urlparse
+
+ALLOWED_HOSTS = {
+    "entel.cl",
+    "ww2.movistar.cl", "movistar.cl",
+    "bci.cl",
+    "caja18.cl", "beneficios.caja18.cl",
+    "sitiospublicos.bancochile.cl"
+}
+
+def is_allowed(url: str) -> bool:
+    try:
+        host = urlparse(url).netloc.replace("www.", "")
+        return host in ALLOWED_HOSTS
+    except:
+        return False
